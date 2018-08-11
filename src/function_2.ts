f4fdfd36011df8e9026299dba41b25ccd4e491f6
durban.zhang <durban.zhang@gmail.com>
2018-08-11 17:43:41 +0800

    add 可选参数和默认参数

---
+++ src/function_2.ts
@@ -0,0 +1,12 @@
+// function buildName(firstName="Bod", lastName: string) {
+//     return firstName + ' ' + lastName
+// }
+
+// // 错误演示
+// buildName("A")
+// // 正确演示
+// buildName(undefined,  "A")
+// // 错误演示
+// buildName("firstName", "lastName", "lastName")
+// // 正确演示
+// buildName("firstName", "lastName")
