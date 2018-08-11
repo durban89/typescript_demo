e9ab11d228b06edd271e325007967d51d3dcbbb2
durban.zhang <durban.zhang@gmail.com>
2018-08-11 16:27:13 +0800

    add func about type

---
+++ src/function_1.ts
@@ -0,0 +1,25 @@
+// function add(x, y) {
+//     return x + y
+// }
+
+// let addFunc = function(x, y) { return x + y }
+
+
+// let z = 10;
+// function addTo(x, y) {
+//     return x + y + z;
+// }
+
+// function add(x: number, y:number): number {
+//     return x + y;
+// }
+
+// let addFunc = function (x: number, y: number): number { return x + y }
+
+// let addFunc: (x: number, y:number) => number = function(x: number, y: number): number { return x + y }
+
+// let addFunc: (baseValue: number, increment: number) => number = function (x: number, y: number): number { return x + y }
+
+// let addFunc = function(x: number, y: number): number { return x + y }
+
+let addFunc: (baseValue: number, increment: number) => number = function(x, y) { return  x + y } 
