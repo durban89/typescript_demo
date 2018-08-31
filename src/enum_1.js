9eab3f55a8ad00ca7c14d21dc172b33d8cfc720c
durban.zhang <durban.zhang@gmail.com>
2018-08-31 13:49:51 +0800

    add 运行时的枚举和外部枚举

---
+++ src/enum_1.js
@@ -0,0 +1,7 @@
+// enum E {
+//     X,Y,Z
+// }
+// function f(obj: { X: number }) {
+//     return obj.X
+// }
+var directions = [0 /* Up */, 1 /* Down */, 2 /* Left */, 3 /* Right */];
---
+++ src/enum_1.ts
@@ -0,0 +1,32 @@
+// enum E {
+//     X,Y,Z
+// }
+// function f(obj: { X: number }) {
+//     return obj.X
+// }
+
+// console.log(f(E));
+
+// enum Enum {
+//     A,
+// }
+
+// let a = Enum.A;
+// let nameOfA = Enum[a];
+// console.log(nameOfA);
+
+// const enum Enum {
+//     A = 1,
+//     B = A * 2,
+// }
+
+const enum Directions {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right]
+
+
