29e02ffb43fbdf6466fec26eae5e981278471277
durban.zhang <durban.zhang@gmail.com>
2018-09-12 15:19:50 +0800

    add 类型别名

---
+++ src/advanced_types_3.ts
@@ -0,0 +1,11 @@
+// type Name = string;
+// type NameFunc = () => string;
+// type NameOrFunc = Name | NameFunc;
+// function getName(n: NameOrFunc): Name {
+//     if (typeof n === 'string') {
+//         return n;
+//     }
+
+//     return n();
+// }
+
