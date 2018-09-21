f632afab239c53b8920d3706f69d393e98d0b853
durban.zhang <durban.zhang@gmail.com>
2018-09-21 11:32:01 +0800

    add 索引类型

--- src/advanced_types_5.ts
+++ src/advanced_types_5.ts
@@ -1,50 +1,50 @@
-class Query {
-  public whereCon: Array<string> = [];
+// class Query {
+//   public whereCon: Array<string> = [];
 
-  public constructor(protected tableName: string = '') { }
+//   public constructor(protected tableName: string = '') { }
 
-  public andWhere(key: string, value: string) {
-    this.whereCon.push(`${key}=${value}`);
-    return this;
-  }
+//   public andWhere(key: string, value: string) {
+//     this.whereCon.push(`${key}=${value}`);
+//     return this;
+//   }
 
-  public orWhere(key: string, value: string) {
-    this.whereCon.push(`OR ${key}=${value}`);
-    return this;
-  }
+//   public orWhere(key: string, value: string) {
+//     this.whereCon.push(`OR ${key}=${value}`);
+//     return this;
+//   }
   
-  public inWhere(key: string, value: string) {
-    this.whereCon.push(`AND ${key} IN (${value})`);
-    return this;
-  }
+//   public inWhere(key: string, value: string) {
+//     this.whereCon.push(`AND ${key} IN (${value})`);
+//     return this;
+//   }
 
-  public getSQL(): string {
-    return `SELECT * FROM ${this.tableName} WHERE ${this.whereCon.join(' ')}`;
-  }
-}
+//   public getSQL(): string {
+//     return `SELECT * FROM ${this.tableName} WHERE ${this.whereCon.join(' ')}`;
+//   }
+// }
 
-let generateSQL = new Query('table_name')
-  .andWhere('key1', 'value1')
-  .orWhere('key2','value2')
-  .inWhere('key3','value3')
-  .getSQL();
+// let generateSQL = new Query('table_name')
+//   .andWhere('key1', 'value1')
+//   .orWhere('key2','value2')
+//   .inWhere('key3','value3')
+//   .getSQL();
 
-console.log(generateSQL);
+// console.log(generateSQL);
 
-class TQuery extends Query {
-  public constructor(tableName: string = '') {
-    super(tableName);
-  }
+// class TQuery extends Query {
+//   public constructor(tableName: string = '') {
+//     super(tableName);
+//   }
 
-  public getUpdateSql(key: string, value: string) {
-    return `UPDATE ${this.tableName} SET ${key}=${value} WHERE ${this.whereCon.join(' ')}`;
-  }
-}
+//   public getUpdateSql(key: string, value: string) {
+//     return `UPDATE ${this.tableName} SET ${key}=${value} WHERE ${this.whereCon.join(' ')}`;
+//   }
+// }
 
-generateSQL = new TQuery('table_name')
-  .andWhere('key1', 'value1')
-  .orWhere('key2', 'value2')
-  .inWhere('key3', 'value3')
-  .getUpdateSql('key4', 'value4');
+// generateSQL = new TQuery('table_name')
+//   .andWhere('key1', 'value1')
+//   .orWhere('key2', 'value2')
+//   .inWhere('key3', 'value3')
+//   .getUpdateSql('key4', 'value4');
 
-console.log(generateSQL);
+// console.log(generateSQL);
