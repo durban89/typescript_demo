a2d76f510f7383a3b37718d7ec2929dfaf52f5af
durban.zhang <durban.zhang@gmail.com>
2018-09-28 09:05:52 +0800

    add interators

---
+++ src/interaters.ts
@@ -0,0 +1,10 @@
+let pets = new Set(["Cat", "Dog", "Hamster"]);
+pets["species"] = "mammals";
+
+for (let pet in pets) {
+  console.log(pet); // "species"
+}
+
+for (let pet of pets) {
+  console.log(pet); // "Cat", "Dog", "Hamster"
+}
